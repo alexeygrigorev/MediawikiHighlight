bd655a864044004005fd6c3b3b15ebeec6f97dd2
Alexey Grigorev <alexey.s.grigoriev@gmail.com>
2015-11-07 00:27:32 +0100

    updates

--- MediawikiHighlight.php
+++ MediawikiHighlight.php
@@ -19,20 +19,35 @@ $wgExtensionCredits['parserhook'][] = array(
     'url'         => 'https://github.com/alexeygrigorev/MediawikiHighlight'
 );
 
-// global $wgHooks;
-// $wgHooks['BeforePageDisplay'][]  = 'setupHeadersForHighlight';
  
 // function setupHeadersForHighlight($out) {
 //     $snippet = file_get_contents(dirname(__FILE__) . '/snippet.html', false);
 //     $out->addScript(eval($snippet));
 // }
 
-$wgResourceModules['ext.MediawikiHighlight'] = array(
+$wgResourceModules['ext.MediawikiHighlight.init'] = array(
     'scripts' => 'highlight.pack.js',
     'styles' => 'styles/github.css',
     'position' => 'bottom',
     'localBasePath' => __DIR__,
     'remoteExtPath' => 'MediawikiHighlight',
 );
+
+global $wgHooks;
+$wgHooks['BeforePageDisplay'][] = 'onBeforePageDisplay';
+
+function onBeforePageDisplay(&$out, &$skin) {
+    $out->addModules('ext.MediawikiHighlight.init');
+    return true;
+}
+
+$wgHooks['BeforePageDisplay'][] = 'ololo';
+
+function ololo(&$out, &$skin) {
+    $out->addScript('<link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/highlight.js/8.9.1/styles/default.min.css">');
+    $out->addScript('<script src="//cdnjs.cloudflare.com/ajax/libs/highlight.js/8.9.1/highlight.min.js"></script>');
+    return true;
+}
+
 
 ?>
